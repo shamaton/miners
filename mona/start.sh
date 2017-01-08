db663d8659f05833cc93fea5e83ad8d2b3ecdff2
shamaton <shamaton77@gmail.com>
2017-01-08 12:20:23 +0900

    fix typo

--- mona/start.sh
+++ mona/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "userpass -> $USERPASS"
-echo "thead    -> $THREAD_NUM"
+echo "thread   -> $THREAD_NUM"
 /root/cpuminer-multi/cpuminer -a lyra2rev2 -o stratum+tcp://vippool.net:8888 --userpass=$USERPASS -t $THREAD_NUM
